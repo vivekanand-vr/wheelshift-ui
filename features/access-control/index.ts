54407fb13a4a6c3dfac4a5cb5aabda19b47f536d
vivekanand-vr <bendragon21@gmail.com>
2026-03-14 22:35:14 +0530

    fix: resolve export type issue

--- features/access-control/index.ts
+++ features/access-control/index.ts
@@ -5,5 +5,5 @@
 export * from "./api";
 export * from "./components";
 export * from "./types";
-export * from "./hooks";
 export * from "./utils";
+export { useEmployeeCustomPermissions } from "./hooks";
